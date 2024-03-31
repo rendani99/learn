f32c3c3cdf50ace00670729c95c73d6e5e5c938d
rendani99 <rendania460@gmail.com>
2024-03-31 18:09:28 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 export default function App() {
   return (
-    <View>
-      <InputText />
-      <button />
+    <View style={{ padding: 60 }}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Add Your Note Here" />
+        <Button color="#000" title="Add Note" />
+      </View>
+      <View>
+        <Text>Show Lists Here!</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    flex: 1,
+  },
+});
